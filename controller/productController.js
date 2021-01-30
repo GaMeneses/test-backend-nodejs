6afada02313d7b03e4c745b48e23edaf3d3a26cb
GaMeneses <gabriel_costacurta@hotmail.com>
2021-01-29 21:46:29 -0300

    add controller

---
+++ controller/productController.js
@@ -0,0 +1,76 @@
+const product = require('../model/product')
+
+
+// adiciona um produto 
+const add = async(req, res) => {
+    try {
+        const { title, description, price, category } = req.body;
+
+        const result = await product.insert(title, description, price, category);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'wrong' });
+    }
+};
+
+const getNameCategory = async(req, res) => {
+    try {
+        const result = await product.getNameCategory(req.query);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'wrong Name or Category !!' });
+    }
+};
+
+// Retorna todos os produtos
+const getAll = async(req, res) => {
+    try {
+
+        const result = await product.getAll();
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'wrong !!' });
+    }
+};
+
+// deleta um produto
+const Delete = async(req, res) => {
+    try {
+
+        const result = await product.Delete(req.params.id);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'wrong !!' });
+    }
+};
+
+
+//Atualiza a categoria do produto
+const update = async(req, res) => {
+    try {
+        const result = await product.update(req.params.id, req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'wrong !!' });
+    }
+};
+
+// Retorna um produto dado o ID
+const getId = async(req, res) => {
+    try {
+
+        const result = await product.getId(req.params.id);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'wrong !!' });
+    }
+};
+module.exports = {
+    add,
+    getAll,
+    Delete,
+    update,
+    getId,
+    getNameCategory
+
+};
