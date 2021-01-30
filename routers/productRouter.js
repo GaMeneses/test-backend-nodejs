ccc38f6c0c533834e1285587e03c12226294fa6f
GaMeneses <gabriel_costacurta@hotmail.com>
2021-01-29 21:46:16 -0300

    add router

---
+++ routers/productRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const product = require('../controller/productController')
+const router = express.Router();
+
+router.get('/filter', product.getNameCategory);
+router.get('/', product.getAll); // Rota para obter todos produtos
+router.post('/', product.add); // Rota para adicionar um produto
+router.get('/:id', product.getId); // Rota para obter um produto pelo ID
+router.put('/:id', product.update); // Rota para atualizar um produto(Categoria) dado o ID
+router.delete('/:id', product.Delete); // Rota para deletar um produto dado o ID
+
+
+module.exports = router;
